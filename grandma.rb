a5d199f62855b32b9253d5318f3af52ee009aad2
Cam Nguyen <cnguyen1187@gmail.com>
2017-12-21 23:55:26 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,8 +13,8 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA"
-    return "I LOVE YOU TOO PUMPKIN"
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
   else
